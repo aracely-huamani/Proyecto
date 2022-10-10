e05d90b78f15d6aa62194b3f15f601e54d9e3d8e
aracely-huamani <102335298+aracely-huamani@users.noreply.github.com>
2022-10-10 16:13:05 -0500

    Add files via upload

---
+++ src/main/java/com/huamani/larico/proyectio/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.huamani.larico.proyectio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
